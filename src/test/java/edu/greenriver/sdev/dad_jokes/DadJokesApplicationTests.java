98e8e986dc549ae4d65ceed68b0cf962f4f7fbed
Ian Schröeder <ianhogan1922@gmail.com>
2024-01-18 14:27:21 -0800

    Initial Commit of files.

---
+++ src/test/java/edu/greenriver/sdev/dad_jokes/DadJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.dad_jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
